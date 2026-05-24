852a7660e2b2e4455273016b6ade3718d84b41bf
Matthew Somerville <matthew@mysociety.org>
2026-05-24 15:32:48 +0100

    Initial commit.

---
+++ conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
+# -- Project information
+
+project = 'Searchable transcripts of the Post Office Horizon IT Inquiry hearings'
+copyright = 'Crown copyright, OGL v3.0'
+author = 'Post Office Horizon IT Inquiry (HTMLized by Matthew Somerville)'
+
+# -- General configuration
+
+extensions = [
+    "recommonmark",
+    "sphinxcontrib.jquery",
+]
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+html_show_sphinx = False
+
+html_theme_options = {
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': '#48466d',
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 0,
+    'includehidden': False,
+    'titles_only': False
+}
+
+html_static_path = ['_static']
+html_css_files = ['custom.css']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ index.rst
@@ -0,0 +1,11 @@
+More searchable evidence of the Post Office Horizon IT Inquiry hearings
+=======================================================================
+
+I have run all the evidence PDFs through tesseract and uploaded them here.
+These pieces of evidence will now be returned through the search as well as
+the transcripts, but you can now also search evidence/transcripts separately.
+
+.. toctree::
+   :glob:
+
+   ./*
